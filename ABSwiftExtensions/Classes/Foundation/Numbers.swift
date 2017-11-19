ef83d69c538dbdb9e6d56f2e82ca12588c36b184
Alex Burtnik <alexander.b@smartum.pro>
2017-11-20 02:09:21 +0300

    Warnings fixed

--- ABSwiftExtensions/Classes/Foundation/Numbers.swift
+++ ABSwiftExtensions/Classes/Foundation/Numbers.swift
@@ -13,11 +13,11 @@ public func trim<T: Comparable>(_ value: T, from: T, to: T) -> T {
     return min(max(value, from), to)
 }
 
-public func trim<T: Comparable>(_ value: T, range: ClosedRange<T>) -> T {
+public func trim<T>(_ value: T, range: ClosedRange<T>) -> T {
     return trim(value, from: range.lowerBound, to: range.upperBound)
 }
 
-public func intersect<T: Comparable>(_ range1: ClosedRange<T>, _ range2: ClosedRange<T>) -> ClosedRange<T> {
+public func intersect<T>(_ range1: ClosedRange<T>, _ range2: ClosedRange<T>) -> ClosedRange<T> {
     return max(range1.lowerBound, range2.lowerBound)...min(range1.upperBound, range2.upperBound)
 }
 
